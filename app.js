04057dab12ff08806a32c5ef010c8412273acb1b
zainktk56 <zaincreationz56@gmail.com>
2026-09-01 19:57:29 +0500

    chp 17-20 complrted

--- app.js
+++ app.js
@@ -1092,6 +1092,177 @@
 // let getdiv=document.getElementById("output");
 // getdiv.innerHTML+=`${selectHTML}`
 
-////
+// Chapter NO 17-20
+
+// 1. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
 
 
+// let mainArray = [
+//     ["a","b","c"],
+//     ["d","e","f"],
+//     ["g","h","i"],
+// ]
+
+// 2. Declare and initialize a multidimensional array
+// representing the following matrix:
+
+// let mainArray = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ]
+
+// let getOutput = document.getElementById("output");
+
+// for (let i = 0; i < mainArray.length; i++) {
+//     for (let j = 0; j < mainArray[i].length; j++) {
+//         getOutput.innerHTML += mainArray[i][j] + " ";
+//     }
+//     getOutput.innerHTML += "<br>";
+// }
+
+// 3.Write a program to print numeric counting from 1 to 10.
+
+// for (let i = 1; i < 11; i++) {
+//     const element = i;
+//     console.log(i)
+// }
+
+//4. Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+// let num=Number(prompt("Enter a Number fot it's table : "));
+
+// for (let i = 1; i < 16; i++) {
+//     const element = ` ${num} x ${i} = ${num*i}` ;
+//     console.log(element)
+// }
+
+
+// 5. Write a program to print items of the following array
+// using for loop:
+
+
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+// “strawberry”]
+
+// let array=["apple","banana","orange","mango","strawbery"]
+// for (let index = 0; index < array.length; index++) {
+//     const element =`Element at index ${index} is ${array[index]}`;
+//     console.log(element)
+// }
+
+
+//6. Generate the following series in your browser. See
+// example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+
+
+// let getpara= document.getElementById("output");
+
+// for (let i = 1; i <16; i++) {
+//     const element = i;
+//     getpara.innerHTML+=` ${element}`
+
+    
+// }
+
+
+// let getpara2=document.getElementById("reverse");
+
+// for (let i = 10; i>=1; i--) {
+//     const reverse_cont = i;
+//     console.log(reverse_cont)
+//     getpara2.innerHTML+=`${reverse_cont} `
+// }
+
+// let getpara3=document.getElementById("even");
+// let getpara4=document.getElementById("odd")
+// for (let i = 0;i<=10; i++) {
+//    if (i%2==0){
+//     getpara3.innerHTML+=` ${i}`
+//    }
+//    else{
+//     getpara4.innerHTML+=` ${i}`
+//    }
+// }
+
+// let getpara5 = document.getElementById("series");
+// for (let i = 1; i <=21; i++) {
+//     if(i%2==0){
+//         getpara5.innerHTML+=` ${i}k`
+//     }
+    
+// }
+
+
+
+//7. You have an array
+// A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+// Write a program to enable “search by user input” in an
+// array.
+// After searching, prompt the user whether the given item is
+// found in the list or not. Example:
+
+
+// let array=['apple','pie','cookie','chips','patties']
+
+// let user_inp=prompt("Welcom To ABC !\n What do you want to Order ?").toLowerCase();
+
+
+// let index = array.indexOf(user_inp);
+// if(index !== -1){
+//     alert(`The ${user_inp} exist in our array at ${index}`)
+// }
+// else{
+//     alert("We are sorry this item is not in or store!")
+    
+// }
+
+
+//8. Write a program to identify the largest number in the
+// given array.
+// A = [24, 53, 78, 91, 12].
+
+// let A = [24, 53, 78, 91, 12];
+// let largest=A[0]
+
+// for (let i = 0; i < A.length; i++) {
+//     const element = A[i];
+//     if (element > largest) {
+//         largest = element;
+//     }
+// }
+// console.log(`Array items: ${A}`);
+// console.log(`The largest number is ${largest}`);
+
+//9. Write a program to identify the smallest number in the
+// given array.
+// A = [24, 53, 78, 91, 12]
+
+//  A = [24, 53, 78, 91, 12]
+
+
+// let smallest=A[0];
+
+// for (let i = 0; i < A.length; i++) {
+//     const element = A[i];
+//     if (element<smallest){
+//         smallest=element
+//     }
+// }
+// console.log(`Array items: ${A}`)
+// console.log(`The smallest number is ${smallest}`);
+
+// 10. Write a program to print multiples of 5 ranging 1 to
+// 100.
+
+// for (let i = 5; i < 100; i += 5) {
+//     console.log(i);
+// }
